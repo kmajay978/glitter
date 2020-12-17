3e41eb2123b6c3e7dc33cc670a44fb3dd029cec7
richestsoft <richestsoft@gmail.com>
2020-12-17 11:05:20 +0530

    changes12312312

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -6,31 +6,6 @@ import Slider from '@material-ui/core/Slider';
 import { makeStyles } from '@material-ui/core/styles';
 import { FILTER_LIST_API } from './Api';
 
-const filterHandle = () =>{
-
-  const bodyParameters = {
-     session_id: localStorage.getItem('session_id'),
-     age_from: setValueAge[0],
-     age_to: setValueAge[1],
-     distance: setValueDistance,
-     height_from:setValueHeight[0],
-     height_to:setValueHeight[1],
-     weight_from:setValueWidth[0],
-     weight_to:setValueWidth[1],
-
-   };
-
-     axios.post(FILTER_LIST_API,bodyParameters)
- .then((response) => { 
-
-     console.log(FILTER_LIST_API);
-   
- 
- }, (error) => {
-   
-    localStorage.clear();
- });
-}
 
 const useStyles = makeStyles({
   root: {
@@ -51,6 +26,8 @@ function valuetextWidth(value) {
   return '${valueWidth}°C';
 }
 const SideFilter = () =>{
+
+
   
   
   const [valueHeight, setValueHeight] = useState([20, 37]);
@@ -69,33 +46,70 @@ const SideFilter = () =>{
       setValueAge(newValue);
      
     };
-    console.log(valueAge);
+   
 
     const [valueDistance, setValueDistance] = useState(30);
     const handleChangeDistance = (event, newValue) => {
     setValueDistance(newValue);
-    // console.log(newValue);
+  
   };
+
+  // Radio button value
+ const [valueGender, setGender] = useState(3);
+  const radioHandle = (e) =>{
+        setGender(e.target.value);
+  }
+
+  const filterHandle = (e) =>{
+        e.preventDefault();
+
+  const bodyParameters = {
+     session_id: localStorage.getItem('session_id'),
+     age_from: valueAge[0],
+     show:valueGender,
+     age_to: valueAge[1],
+     distance: valueDistance,
+     height_from:valueHeight[0],
+     height_to:valueHeight[1],
+     weight_from:valueWidth[0],
+     weight_to:valueWidth[1],
+     latitude:30.7,
+     longitude:76.0,
+   
+
+   };
+
+     axios.post(FILTER_LIST_API,bodyParameters)
+ .then((response) => { 
+
+     console.log(response);
+   
+ 
+ }, (error) => {
+   
+    localStorage.clear();
+ });
+}
 
   return(
 
         <div className="filter-tab">
                   <h4 className="mb-4">Filter</h4>
                   <form action="#" method="post">
-                    <div className="show-gender ft-block d-flex flex-wrap">
+                    <div className="show-gender ft-block d-flex flex-wrap" onChange={radioHandle}>
                       <div className="tab-title">
                         <h5>Show Me</h5>
                       </div>
                       <div className="form-group">
-                        <input type="checkbox" defaultChecked name="man" id="man" />
+                        <input type="radio" defaultChecked name="gender" value={1} id="man" />
                         <label htmlFor="man">Man</label>
                       </div>
                       <div className="form-group">
-                        <input type="checkbox" name="woman" id="woman" />
+                        <input type="radio" name="gender" value={2} id="woman" />
                         <label htmlFor="woman">Woman</label>
                       </div>
                       <div className="form-group">
-                        <input type="checkbox" defaultChecked name="both" id="both" />
+                        <input type="radio" defaultChecked name="gender" value={3} id="both" />
                         <label htmlFor="both">Both</label>
                       </div>
                     </div>
@@ -103,16 +117,18 @@ const SideFilter = () =>{
                       <div className="tab-title">
                         <h5>Age</h5>
                       </div>
+                      <input type="text" className="two-range" id="age" readOnly />
                       <Slider value={valueAge} onChange={handleChangeAge} valueLabelDisplay="auto"
             aria-labelledby="range-slider" getAriaValueText={valuetextAge}/>
                     </div>
 
                     <div className="distance-group ft-block">
                       <div className="tab-title"> 
-                        <h5>Distance</h5>
+                        <h5>Distance</h5> 
                       </div>
                       <div className="range-slider">
-                      <Slider value={valueDistance} onChange={handleChangeDistance} aria-labelledby="continuous-slider" />
+                      <Slider value={valueDistance} onChange={handleChangeDistance} valueLabelDisplay="auto"
+            aria-labelledby="range-slider" aria-labelledby="continuous-slider" />
                       </div>
                     </div>
                     <div className="height-group ft-block">
@@ -133,7 +149,7 @@ const SideFilter = () =>{
         aria-labelledby="range-slider" getAriaValueText={valuetextWidth}/>
                     </div>
                     <div className="btns-group d-flex justify-content-between flex-wrap my-5">
-                      <button className="btn bg-grd-clr" type="submit">Done</button>
+                      <button className="btn bg-grd-clr" type="submit" onClick={filterHandle}>Done</button>
                       <button className="btn bg-grd-clr" type="reset">Reset</button>
                     </div>
                   </form>
