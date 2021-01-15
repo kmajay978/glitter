b48d61a64de710970bf4ee7c7aad2e337eac4000
richestsoft <richestsoft@gmail.com>
2021-01-15 19:14:21 +0530

    added chat

--- src/components/Config.js
+++ src/components/Config.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
-export const SOCKET = io('http://192.168.0.196:3001/', {
+export const SOCKET = io('http://3.17.161.217:3001/', {
     // autoConnect: false,
     // reconnectionDelay: 1000,
     // reconnection: true,
