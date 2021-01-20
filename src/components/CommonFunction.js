657c145443a3a3a4c600623888c9e243bd223541
richestsoft <richestsoft@gmail.com>
2021-01-20 16:37:41 +0530

    added location

--- src/components/CommonFunction.js
+++ src/components/CommonFunction.js
@@ -37,6 +37,34 @@ export function randomString(len = 5) {
 	return text;
 }
 
+// Geo location 
+export const usePosition = () => {
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+  
+  const onChange = ({coords}) => {
+    setPosition({
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+    });
+  };
+  const onError = (error) => {
+    setError(error.message);
+  };
+  useEffect(() => {
+    const geo = navigator.geolocation;
+    if (!geo) {
+      setError('Geolocation is not supported');
+      return;
+    }
+    watcher = geo.watchPosition(onChange, onError);
+    return () => geo.clearWatch(watcher);
+  }, []);
+  return {...position, error};
+}
+
+
+
 
 
 
