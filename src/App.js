2b8968a25072f070487b5c5ff11287964812b098
unknown <kmajay978>
2021-02-10 16:34:05 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -61,7 +61,9 @@ function App() {
       if (!!userData && (data.user_from_id == userData.user_id)) { // check one-to-one data sync
         alert("receiver declined your call...")
       }
-      history.push("/chat")
+      if (!!userData && ((data.user_from_id == userData.user_id) || (data.user_to_id == userData.user_id))) {
+        history.push("/chat")
+      }
     })
     SOCKET.on('sender_decline_video_call', (data) => {
       localStorage.removeItem("videoCallPageRefresh");
@@ -70,7 +72,9 @@ function App() {
       if (!!userData && (data.user_to_id == userData.user_id)) { // check one-to-one data sync
         alert("sender declined the call...")
       }
-      history.push("/chat")
+      if (!!userData && ((data.user_to_id == userData.user_id) || (data.user_from_id == userData.user_id))) {
+        history.push("/chat")
+      }
     })
     SOCKET.on('call_not_picked_receiver_hide_page_video_call', (data) => {
       localStorage.removeItem("videoCallPageRefresh");
